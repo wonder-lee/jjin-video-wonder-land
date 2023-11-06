c77c85ee5363821f412e5071e93eb923194ef7b1
wonder-lee <hi.wonder.dev@gmail.com>
2023-11-07 00:11:52 +0900

    [231107] searchInput > airbnb style로 수정

--- app/components/searchInput.tsx
+++ app/components/searchInput.tsx
@@ -17,6 +17,19 @@ export default SearchInput;
 
 const Style = {
   SearchInputContainer: styled(Search)`
+    padding: 15px 25px 0px 25px;
+
+    input {
+      height: 45px;
+      border-radius: 50px;
+      box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 12px;
+    }
+    .ant-btn {
+      border-top-right-radius: 50px !important;
+      border-bottom-right-radius: 50px !important;
+      box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 12px !important;
+    }
+
     .ant-input,
     .ant-btn {
       height: 50px;
