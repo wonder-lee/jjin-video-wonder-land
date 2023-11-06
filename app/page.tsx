e5b48f5855dfb19015ddb170b456dbb938edcb65
wonder-lee <hi.wonder.dev@gmail.com>
2023-11-07 00:15:02 +0900

    [231107] videoCard click > component 적용
    - videoCard click하면 어떤 component를 보여줄 지 test 중

--- app/page.tsx
+++ app/page.tsx
@@ -5,37 +5,53 @@ import SearchInput from "./components/searchInput";
 import FilterSelect from "./components/filterSelect";
 import VideoCard from "./components/videoCard";
 import VideoCardModal from "./components/videoCardModal";
+import FilterDropdown from "./components/filterDropdown";
+import ShareDrawer from "./components/shareDrawer";
 import { FILTERS } from "./constants/filters";
 import { RESPONSE_EXAMPLE } from "./constants/responseExample";
 
 const Home = function Home() {
   const [selectedCardIndex, setSelectedCardIndex] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <Style.MainContainer>
-        <Style.VideoCardListContainer>
+        <Style.VideoCardListArea>
           {RESPONSE_EXAMPLE.list.map((data, index) => (
             <VideoCard
               data={data}
               index={index}
-              setIsModalOpen={setIsModalOpen}
+              setIsModalOpen={setOpen}
               setSelectedCardIndex={setSelectedCardIndex}
             />
           ))}
-        </Style.VideoCardListContainer>
-        <Style.FiltersContainer>
-          {FILTERS.map((FILTER) => (
-            <FilterSelect options={FILTER} />
-          ))}
-        </Style.FiltersContainer>
-        <SearchInput />
-        <VideoCardModal
-          data={RESPONSE_EXAMPLE.list[selectedCardIndex]}
-          isModalOpen={isModalOpen}
-          setIsModalOpen={setIsModalOpen}
-        />
+        </Style.VideoCardListArea>
+        <ShareDrawer open={open} setOpen={setOpen} />
       </Style.MainContainer>
+
+      <Style.UserControllerContainer>
+        <Style.UserControllerArea>
+          {isModalOpen ? (
+            <div>hello</div>
+          ) : (
+            <>
+              <Style.FiltersArea>
+                <Style.FiltersScroll>
+                  {FILTERS.map((FILTER) => {
+                    const { items, icon, name } = FILTER;
+                    return (
+                      <FilterDropdown items={items} icon={icon} name={name} />
+                    );
+                  })}
+                </Style.FiltersScroll>
+              </Style.FiltersArea>
+              <SearchInput />
+            </>
+          )}
+        </Style.UserControllerArea>
+      </Style.UserControllerContainer>
     </>
   );
 };
@@ -46,19 +62,29 @@ const Style = {
   MainContainer: styled.main`
     max-width: 375px;
     margin: 0 auto;
-    padding: 0 24px 14px;
+    padding: 15px 25px;
   `,
-  FiltersContainer: styled.div`
+  VideoCardListArea: styled.div`
+    margin-bottom: 200px;
+  `,
+  UserControllerContainer: styled.div`
+    position: fixed;
     display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    gap: 10px;
-    max-width: 420px;
-    margin: 15px 0px;
+    justify-content: center;
+    background-color: white;
+    bottom: 0px;
+    width: calc(100vw - 20px);
   `,
-  VideoCardListContainer: styled.div`
-    overflow-y: scroll;
-    height: calc(100vh - 250px);
-    padding: 0px 35px;
+  UserControllerArea: styled.div`
+    max-width: 375px;
+    padding: 25px 0px;
+  `,
+  FiltersArea: styled.div`
+    /* overflow-x: scroll; */
+    padding: 0px 20px;
+  `,
+  FiltersScroll: styled.div`
+    min-width: 375px;
+    display: flex;
   `,
 };
