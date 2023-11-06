d90ae9c62c77188ca2737ab58c5719a7dd34e4d4
wonder-lee <hi.wonder.dev@gmail.com>
2023-11-07 00:12:17 +0900

    [231107] Drawer component 생성
    - 임시 생성

---
+++ app/components/shareDrawer.tsx
@@ -0,0 +1,41 @@
+import React, { Dispatch, useState } from "react";
+import type { DrawerProps, RadioChangeEvent } from "antd";
+import { Button, Drawer, Radio, Space } from "antd";
+
+type PropsType = {
+  open: boolean;
+  setOpen: Dispatch<boolean>;
+};
+
+const ShareDrawer = ({ open, setOpen }: PropsType) => {
+  const [placement, setPlacement] = useState<DrawerProps["placement"]>("left");
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const onChange = (e: RadioChangeEvent) => {
+    setPlacement(e.target.value);
+  };
+  return (
+    <div>
+      <Drawer
+        title="Basic Drawer"
+        placement="bottom"
+        closable={false}
+        onClose={onClose}
+        open={open}
+        key={placement}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </div>
+  );
+};
+export default ShareDrawer;
