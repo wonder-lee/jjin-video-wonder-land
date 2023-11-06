0fcaade821d27c636419e97717e8c81c45d61fbc
wonder-lee <hi.wonder.dev@gmail.com>
2023-11-07 00:13:19 +0900

    [231107] filterDropdown > icon 추가 및 data 수정

--- app/components/filterDropdown.tsx
+++ app/components/filterDropdown.tsx
@@ -1,18 +1,38 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { Dropdown, Button } from "antd";
 import type { MenuProps } from "antd";
+import styled from "styled-components";
 
 type PropsType = {
   items: MenuProps["items"];
+  icon: any;
   name: string;
 };
 
-const FilterDropdown = ({ items, name }: PropsType) => {
+const FilterDropdown = ({ items, icon, name }: PropsType) => {
   return (
-    <Dropdown menu={{ items }} trigger={["click"]} placement="bottomLeft">
-      <Button>bottomLeft</Button>
-    </Dropdown>
+    <Style.FilterDropdownContainer menu={{ items }} trigger={["click"]}>
+      <Button>
+        <div>{icon}</div>
+        <div>{name}</div>
+      </Button>
+    </Style.FilterDropdownContainer>
   );
 };
 
 export default FilterDropdown;
+
+const Style = {
+  FilterDropdownContainer: styled(Dropdown)`
+    width: 85px;
+    height: auto;
+    border-color: white;
+    box-shadow: unset;
+    font-size: 12px;
+    color: #8c8c8c;
+
+    svg {
+      font-size: 20px;
+    }
+  `,
+};
