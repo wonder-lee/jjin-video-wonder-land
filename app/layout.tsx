1d34eae2e3d00cce9ee396b4f243fb58332ad052
wonder-lee <hi.wonder.dev@gmail.com>
2023-11-08 00:29:57 +0900

    [231108] Noto_Sans_KR 폰트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import StyledComponentsRegistry from "../lib/AntdRegistry";
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans_KR } from "next/font/google";
 import { ConfigProvider } from "antd";
 
 const inter = Inter({ subsets: ["latin"] });
+const notoSerif = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata = {
-  title: "찐영상",
-  description: "유튜브 분석 툴 | 유튜브 영상 분석",
+  title: "찐영상 - 유튜브 영상 분석",
+  description: "가장 가치있는 영상을 찾아드립니다.",
 };
 
 export default function RootLayout({
@@ -15,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="kor">
       <ConfigProvider
         theme={{
           token: {
@@ -23,7 +27,7 @@ export default function RootLayout({
           },
         }}
       >
-        <body className={inter.className}>
+        <body className={notoSerif.className}>
           <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         </body>
       </ConfigProvider>
